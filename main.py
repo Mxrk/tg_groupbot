1e984e3ed2f5a242f2239d88c82cebdb01cf814f
Mxrk <Mxrk@users.noreply.github.com>
2018-10-04 12:17:54 +0200

    Update main.py and remove old utils folder

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 import Config as cfg
 from database.database import Database
 from cmds.admingroup import Admin
+from cmds.utils import Utils
 from filters.welcomeMessage import WelcomeMessage
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -19,6 +20,7 @@ print("Running")
 
 dispatcher.add_handler(CommandHandler('init', Admin.init))
 dispatcher.add_handler(CommandHandler('rules', Admin.rules))
+dispatcher.add_handler(CommandHandler('id', Utils.id))
 dispatcher.add_handler(CallbackQueryHandler(Admin.button))
 dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, WelcomeMessage.greeting))
 
