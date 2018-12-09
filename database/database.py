cb0084ddd3f8b2c13712d1d422e828d20ffc9dfa
Mxrk <Mxrk@users.noreply.github.com>
2018-12-09 21:45:11 +0100

    ADD database lock

--- database/database.py
+++ database/database.py
@@ -1,8 +1,19 @@
 import pymongo
+from threading import Lock
 
 
-class Database:
-    def __init__(self, ip="127.0.0.1", port=27017, dbname="groupbot", collectionname="user"):
+class Database(object):
+    _instance = None
+    _lock = Lock()
+
+    def __new__(cls, *args, **kwargs):
+        with Database._lock:
+            if Database._instance is None:
+                Database._instance = super(Database, cls).__new__(cls)
+
+        return Database._instance
+
+    def __init__(self, ip="127.0.0.1", port=27017, dbname="groupbot", collectionname="groups"):
         self.db = pymongo.MongoClient(ip, port)
         self.db = self.db[dbname]
         self.collection = self.db[collectionname]
